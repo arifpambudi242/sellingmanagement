15b84fc0ce91d0a511094316eff0c7caccab1aa4
Arif Pambudi <arifpambudi242@gmail.com>
2023-11-09 15:54:30 +0700

    sementara

--- app.py
+++ app.py
@@ -4,6 +4,9 @@ from datetime import datetime
 from flask import Flask, render_template, request, redirect, url_for, flash, jsonify, make_response, session
 import dotenv
 import os
+from flask_bcrypt import Bcrypt
+
+bcrypt = Bcrypt()
 
 dotenv.load_dotenv()
 
@@ -25,6 +28,21 @@ class Sempol(db.Model):
     harga = db.Column(db.Float)
     waktu = db.Column(db.DateTime, default=datetime.now)
     # Tambahkan kolom lain sesuai kebutuhan
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(60), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.now)
+
+    def __init__(self, username, email, password):
+        self.username = username
+        self.email = email
+        self.password = bcrypt.generate_password_hash(password).decode('utf-8')
+
+    def check_password(self, password):
+        return bcrypt.check_password_hash(self.password, password)
+
 
 @app.route('/')
 def index():
@@ -44,4 +62,3 @@ if __name__ == '__main__':
     with app.app_context():
         db.create_all()
     app.run(debug=True)
-
